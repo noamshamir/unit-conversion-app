225796f1fa9d801fb2511b3948c940c908ada1a7
noamshamir <noamshamir09.gmail.com>
2022-11-20 20:01:13 -0600

    Added a non-functional dropdown

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 
@@ -24,15 +23,13 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  @override 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Hello World!')), 
-          body: NumberInputWidget(),
-    );  
+      appBar: AppBar(title: Text('Hello World!')),
+      body: NumberInputWidget(),
+    );
   }
-
 }
 
 class NumberInputWidget extends StatefulWidget {
@@ -50,6 +47,8 @@ class _NumberInputWidgetState extends State<NumberInputWidget> {
   double numberValue2 = 0;
   double conversionFactor = 100;
 
+  String dropdownValue = '1';
+
   @override
   void dispose() {
     super.dispose();
@@ -64,13 +63,13 @@ class _NumberInputWidgetState extends State<NumberInputWidget> {
         numberValue2 = 0;
         myController2.text = "";
         return;
-      } 
+      }
       numberValue1 = double.parse(text);
       numberValue2 = numberValue1 / conversionFactor;
       myController2.text = numberValue2.toString();
     });
   }
-  
+
   void convert2(text) {
     setState(() {
       if (text == "-" || text == "") {
@@ -84,30 +83,54 @@ class _NumberInputWidgetState extends State<NumberInputWidget> {
       myController1.text = numberValue1.toString();
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Column(children: <Widget>[
+      DropdownButton<String>(
+          value: dropdownValue,
+          icon: const Icon(Icons.arrow_drop_down),
+          elevation: 16,
+          style: const TextStyle(color: Colors.blue),
+          underline: Container(
+            height: 0,
+            color: Colors.blueAccent,
+          ),
+          onChanged: (String? value) {
+            // This is called when the user selects an item.
+            setState(() {
+              dropdownValue = value!;
+            });
+          },
+          items: const [
+            DropdownMenuItem(child: Text("Meter"), value: "1"),
+            DropdownMenuItem(child: Text("Centimeter"), value: ".01"),
+            DropdownMenuItem(child: Text("Kilometer"), value: "1000"),
+          ]),
       TextField(
-        inputFormatters: [FilteringTextInputFormatter.allow(RegExp(r'(^\-?\d*\.?\d*)'))],
-        keyboardType: TextInputType.numberWithOptions(
-          decimal: true,
-          signed: true),
+        inputFormatters: [
+          FilteringTextInputFormatter.allow(RegExp(r'(^\-?\d*\.?\d*)'))
+        ],
+        keyboardType:
+            TextInputType.numberWithOptions(decimal: true, signed: true),
         controller: myController1,
         decoration: InputDecoration(
-          prefixIcon: Icon(Icons.numbers), labelText: 'type a number already'),
+            prefixIcon: Icon(Icons.numbers),
+            labelText: 'type a number already'),
         onChanged: (text) => convert1(text),
       ),
       TextField(
-        inputFormatters: [FilteringTextInputFormatter.allow(RegExp(r'(^\-?\d*\.?\d*)'))],
-        keyboardType: TextInputType.numberWithOptions(
-          decimal: true,
-          signed: true),
+        inputFormatters: [
+          FilteringTextInputFormatter.allow(RegExp(r'(^\-?\d*\.?\d*)'))
+        ],
+        keyboardType:
+            TextInputType.numberWithOptions(decimal: true, signed: true),
         controller: myController2,
         decoration: InputDecoration(
-          prefixIcon: Icon(Icons.numbers), labelText: 'type a number already'),
+            prefixIcon: Icon(Icons.numbers),
+            labelText: 'type a number already'),
         onChanged: (text) => convert2(text),
       ),
-    ]); 
+    ]);
   }
 }
-
